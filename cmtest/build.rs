6bb4fac1f8ddaee18396ed4cff0dcd93e18cf1e7
Will <willberrisford@gmail.com>
2025-01-07 23:53:22 +0000

    Tracking all defined and undefined symbols

--- cmtest/build.rs
+++ cmtest/build.rs
@@ -1,21 +1,16 @@
 use std::{
-    collections::HashSet,
-    convert::identity,
+    collections::{HashMap, HashSet},
     error::Error,
     ffi::OsStr,
-    fs::{self, File},
+    fmt::Debug,
+    fs::File,
     hash::Hash,
     io::Read,
-    path::{Path, PathBuf},
-    str,
+    path::Path,
     sync::LazyLock,
 };
 
-use cmake;
-use object::{
-    read::archive::{ArchiveFile, ArchiveSymbol},
-    Object, ObjectSymbol,
-};
+use object::{Object, ObjectSymbol};
 use regex::Regex;
 use walkdir::{DirEntry, WalkDir};
 
@@ -33,25 +28,55 @@ fn main() {
     println!("cargo:rerun-if-changed=../tinkwrap");
 
     println!("cargo:rustc-link-search=native={}", lib.display());
+    println!("cargo:rustc-link-lib=static=tinkwrap");
 
-    for lib in find_libs(Path::new(&lib.display().to_string())) {
+    let libs = find_libs(Path::new(&lib.display().to_string()));
+
+    let symbols = generate_lookup_tables(libs.clone());
+    symbols
+        .defined
+        .iter()
+        .for_each(|(symbol, static_lib)| println!("{:?} {:?}", symbol, static_lib));
+
+    symbols
+        .undefined
+        .iter()
+        .for_each(|(defined, static_lib)| println!("{:?} {:?}", defined, static_lib));
+
+    for lib in &libs {
         println!("Found static lib: {}", lib.name);
-        lib.undefined_symbols
-            .into_iter()
-            .map(|bytes| String::from_utf8(bytes).unwrap_or(String::from("Not utf8!")))
-            .for_each(|name| println!("UndefinedSymbol symbol: {}", name));
+        //lib.undefined_symbols
+        //    .into_iter()
+        //    .map(|bytes| String::from_utf8(bytes).unwrap_or(String::from("Not utf8!")))
+        //    .for_each(|name| println!("UndefinedSymbol symbol: {}", name));
 
-        lib.defined_symbols
-            .into_iter()
-            .map(|bytes| String::from_utf8(bytes).unwrap_or(String::from("Not utf8!")))
-            .for_each(|name| println!("DefinedSymbol symbol: {}", name));
+        //lib.defined_symbols
+        //    .into_iter()
+        //    .map(|bytes| String::from_utf8(bytes).unwrap_or(String::from("Not utf8!")))
+        //    .for_each(|name| println!("DefinedSymbol symbol: {}", name));
+    }
+}
+
+fn generate_lookup_tables<I>(libs: I) -> AllSymbols
+where
+    I: IntoIterator<Item = StaticLib>,
+{
+    let mut defined_table: HashMap<DefinedSymbol, StaticLib> = HashMap::new();
+    let mut undefined_table: HashMap<UnDefinedSymbol, StaticLib> = HashMap::new();
+    for lib in libs {
+        for symbol in lib.defined_symbols.clone() {
+            defined_table.insert(symbol, lib.clone());
+        }
+
+        for symbol in lib.undefined_symbols.clone() {
+            undefined_table.insert(symbol, lib.clone());
+        }
     }
 
-    println!(
-        "cargo:rustc-link-search=native={}/build/3rdParty/a/",
-        lib.display()
-    );
-    println!("cargo:rustc-link-lib=static=a");
+    AllSymbols {
+        defined: defined_table,
+        undefined: undefined_table,
+    }
 }
 
 static LIB_REGEX: LazyLock<Regex> =
@@ -77,15 +102,50 @@ fn find_libs(base_path: &Path) -> HashSet<StaticLib> {
         .collect::<HashSet<StaticLib>>()
 }
 
+struct AllSymbols {
+    defined: HashMap<DefinedSymbol, StaticLib>,
+    undefined: HashMap<UnDefinedSymbol, StaticLib>,
+}
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+struct DefinedSymbol {
+    symbol: Vec<u8>,
+}
+
+impl Debug for DefinedSymbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DefinedSymbol")
+            .field(
+                "symbol",
+                &String::from_utf8(self.symbol.clone()).unwrap_or(String::from("Not utf8")),
+            )
+            .finish()
+    }
+}
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+struct UnDefinedSymbol {
+    symbol: Vec<u8>,
+}
+
+impl Debug for UnDefinedSymbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("UnDefinedSymbol")
+            .field(
+                "symbol",
+                &String::from_utf8(self.symbol.clone()).unwrap_or(String::from("Not utf8")),
+            )
+            .finish()
+    }
+}
+
+#[derive(Clone)]
 struct StaticLib {
     name: String,
     entry: DirEntry,
     defined_symbols: Vec<DefinedSymbol>,
     undefined_symbols: Vec<UnDefinedSymbol>,
 }
-
-type DefinedSymbol = Vec<u8>;
-type UnDefinedSymbol = Vec<u8>;
 
 impl Hash for StaticLib {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
@@ -101,6 +161,14 @@ impl PartialEq for StaticLib {
 
 impl Eq for StaticLib {}
 
+impl Debug for StaticLib {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("StaticLib")
+            .field("name", &self.name)
+            .finish()
+    }
+}
+
 fn get_symbols(
     entry: &DirEntry,
 ) -> Result<(Vec<DefinedSymbol>, Vec<UnDefinedSymbol>), Box<dyn Error>> {
@@ -110,7 +178,7 @@ fn get_symbols(
     let mut all_defined: Vec<DefinedSymbol> = vec![];
     let mut all_undefined: Vec<UnDefinedSymbol> = vec![];
 
-    while let Some(mut entry) = archive.next_entry() {
+    while let Some(entry) = archive.next_entry() {
         let Ok(mut entry) = entry else {
             continue;
         };
@@ -123,16 +191,20 @@ fn get_symbols(
         let file = object::File::parse(&*buf)?;
         let mut defined = file
             .symbols()
-            //.filter(|symbol| symbol.is_definition())
+            .filter(|symbol| symbol.is_definition())
             .filter_map(|symbol| symbol.name_bytes().ok())
-            .map(|bytes| bytes.to_vec())
+            .map(|bytes| DefinedSymbol {
+                symbol: bytes.to_vec(),
+            })
             .collect();
 
         let mut undefined = file
             .symbols()
             .filter(|symbol| symbol.is_undefined())
             .filter_map(|symbol| symbol.name_bytes().ok())
-            .map(|bytes| bytes.to_vec())
+            .map(|bytes| UnDefinedSymbol {
+                symbol: bytes.to_vec(),
+            })
             .collect();
 
         all_defined.append(&mut defined);
@@ -140,30 +212,6 @@ fn get_symbols(
     }
 
     Ok((all_defined, all_undefined))
-}
-
-fn get_symbols_from_object(
-    entry: &DirEntry,
-) -> Result<(Vec<DefinedSymbol>, Vec<UnDefinedSymbol>), Box<dyn Error>> {
-    let data = fs::read(entry.path())?;
-    println!("Parsing file {:?}", entry.path());
-    let file = object::File::parse(&*data)?;
-    println!("Parsed file");
-    let defined = file
-        .symbols()
-        //.filter(|symbol| symbol.is_definition())
-        .filter_map(|symbol| symbol.name_bytes().ok())
-        .map(|bytes| bytes.to_vec())
-        .collect();
-
-    let undefined = file
-        .symbols()
-        .filter(|symbol| symbol.is_undefined())
-        .filter_map(|symbol| symbol.name_bytes().ok())
-        .map(|bytes| bytes.to_vec())
-        .collect();
-
-    Ok((defined, undefined))
 }
 
 fn is_static_lib(file_name: &OsStr) -> bool {
